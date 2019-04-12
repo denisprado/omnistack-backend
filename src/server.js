0780009558f64698aa4e990d95e43572440cbdbe
denisforigo <denisforigo@gmail.com>
2019-04-11 22:52:28 -0300

    Bug Fix

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+
 const app = express();
+
 app.use(cors())
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
 io.on('connection', socket => {
-    socket.on('connectio'), box => {
+    socket.on('connectRoom'), box => {
         socket.join(box);
     }
 })
