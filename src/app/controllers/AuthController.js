be7ef85f9fd6880cacd2f3406ac7ad8dc4c3e0fa
denisprado <denisforigo@gmail.com>
2019-04-17 16:23:58 -0300

    user authentication list

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -9,8 +9,8 @@ const jwt = require('jsonwebtoken')
 const authConfig = require('../../config/auth')
 
 function generateToken(params = {}) {
-    return jwt.sign({ id: user.id }, authConfig.secret, {
-        expiresIn: 86400
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
     })
 }
 
@@ -30,27 +30,29 @@ router.get('/list', function (req, res) {
 router.post('/register', async (req, res) => {
     const { email } = req.body;
     try {
-        if (await User.findOne({ email })) {
+        if (await User.findOne({ email }))
             return res.status(400).send({ error: "User alredy existis" })
-        }
+
         const user = await User.create(req.body)
 
         user.password = undefined;
 
-        return res.send({ user, token: generateToken({ id: user.id }) })
+        return res.send({ 
+            user, 
+            token: generateToken({ id: user.id }) })
     } catch (err) {
         return res.status(400).send({ error: "Registration failed" })
     }
 })
 
 router.post('/authenticate', async (req, res) => {
-    const { email, password } = res.body;
+    const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password')
 
-    if (!user) {
-        return res.status(400).status({ error: "User not found" })
-    }
+    if (!user)
+        return res.status(400).send({ error: "User not found" });
+
 
     //compara a senha digitada com a do banco de dados
     if (!await bcrypt.compare(password, user.password))
