f198e5562949c6dec89a94a7761950678dc0ab04
denisprado <denisforigo@gmail.com>
2019-04-17 17:03:18 -0300

    user authentication list

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -8,14 +8,14 @@ module.exports = (req, res, next) => {
         return res.status(400).send({ error: "No token provided" })
 
     //Bearer
-    const parts = authHeader.splice(' ')
+    const parts = authHeader.split(' ')
 
     if (!parts.lenght === 2)
         return res.status(401).send({ error: "Token error" })
 
     const [scheme, token] = parts
 
-    if (!/^Bearer$^/i.text(scheme))
+    if (!/^Bearer$/i.test(scheme))
         return res.status(401).send({ error: "Token malformatted" })
 
     jwt.verify(token, authConfig.secret, (err, decoded) => {
